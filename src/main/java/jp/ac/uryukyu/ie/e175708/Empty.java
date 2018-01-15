e7d2511a3819edfdb0ca124ae588dd1575b9d1bf
MatsuMatsu0129 <e175708@ie.u-ryukyu.ac.jp>
2018-01-16 01:27:42 +0900

    Emptyクラス実装

---
+++ src/main/java/jp/ac/uryukyu/ie/e175708/Empty.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.e175708;
+
+public class Empty {
+    private String emp[];
+    public Empty() {
+        MkEmpty();
+
+    }
+
+
+    public void MkEmpty() {
+        emp = new String[52];
+        for (int i = 0; i < emp.length; i++){
+            emp[i] = "?";
+        }
+    }
+}
+
